b14ec702e72599c03fecf852d9427203ff685d25
pharell <86664849+Pharell77coder@users.noreply.github.com>
2025-03-12 17:26:55 +0100

    sql

--- sql/bdd_commerce.sql
+++ sql/bdd_commerce.sql
@@ -24,6 +24,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `video` varchar(255) NOT NULL,
+  `price` double NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `duration` varchar(255) NOT NULL,
+  `upload_date` date NOT NULL,
+  `type` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `image`, `video`, `price`, `description`, `duration`, `upload_date`, `type`) VALUES
+(1, 'Produit 1', 'images/produit1.jpeg', 'videos\\video.mp4', 20, 'Manga edit', '1m00s', '2025-03-05', 'CSS'),
+(2, 'Produit 2', 'images/produit2.jpeg', 'videos\\video.mp4', 25, 'Manga edit', '1m00s', '2025-03-05', 'JS'),
+(3, 'Produit 3', 'images/produit3.jpeg', 'videos\\video.mp4', 30, 'Manga edit', '1m00s', '2025-03-05', 'PHP');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `users`
 --
 
@@ -45,6 +72,12 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `users`
--- sql/products.sql
+++ sql/products.sql
@@ -24,43 +24,39 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `users`
+-- Structure de la table `products`
 --
 
-CREATE TABLE `users` (
-  `id` int(255) NOT NULL,
-  `username` varchar(30) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `video` varchar(255) NOT NULL,
+  `price` double NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `duration` varchar(255) NOT NULL,
+  `upload_date` date NOT NULL,
+  `type` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Déchargement des données de la table `users`
+-- Déchargement des données de la table `products`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
-(0, 'Administrateur', 'titi.pharell77420@gmail.com', '$2y$10$JlmwQ9SbX3r4S0TNQtnsDO5GwUeHtpzBT9bHBOuyoO/bk4tDh0R7K'),
-(1, 'test', 'test@email.com', '$2y$10$iYNU6it2bdR8iBCSsDHdbOdXDSUKb5/nASBkgDpsUsfn3R67.8GY2');
+INSERT INTO `products` (`id`, `name`, `image`, `video`, `price`, `description`, `duration`, `upload_date`, `type`) VALUES
+(1, 'Produit 1', 'images/produit1.jpeg', 'videos\\video.mp4', 20, 'Manga edit', '1m00s', '2025-03-05', 'CSS'),
+(2, 'Produit 2', 'images/produit2.jpeg', 'videos\\video.mp4', 25, 'Manga edit', '1m00s', '2025-03-05', 'JS'),
+(3, 'Produit 3', 'images/produit3.jpeg', 'videos\\video.mp4', 30, 'Manga edit', '1m00s', '2025-03-05', 'PHP');
 
 --
 -- Index pour les tables déchargées
 --
 
 --
--- Index pour la table `users`
+-- Index pour la table `products`
 --
-ALTER TABLE `users`
+ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
